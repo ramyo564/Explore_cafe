8f9041d83378aa6245a6e918c8cd5379dc713521
ramyo564 <yohan032yohan@gmail.com>
2023-06-20 06:23:04 +0900

    core settings

--- core/urls.py
+++ core/urls.py
@@ -19,5 +19,6 @@ from django.urls import path,include
 from. import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name="home")
+    path('', views.home, name="home"),
+    path('explore', include('explore.urls')),
 ]
